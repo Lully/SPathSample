a315426780326f008ec374f59ae2a6824c20e5e9
Lullly <lully1804@yahoo.fr>
2018-10-01 18:35:23 +0200

    split files

--- sparqllevel6dict.py
+++ sparqllevel6dict.py
@@ -4,7 +4,8 @@ Created on Wed Sep 26 14:55:28 2018
 
 @author: lapotre
 """
-
+from zipfile import ZipFile
+from os import remove
 import pandas as pd
 from SPARQLWrapper import SPARQLWrapper, JSON, SPARQLExceptions
 import json
@@ -12,15 +13,13 @@ import json
 errors = open("errors.txt", "w", encoding="utf-8")
 
 #data = pd.read_csv('/home/lapotre/articles_blog/inriaviz/sampleSPath.csv')
-data = pd.read_csv('sampleSPath.csv')
-#data = pd.read_csv('test.csv')
+#data = pd.read_csv('sampleSPath.csv')
+data = pd.read_csv('test.csv')
 
-final_dict = {}
 sparql = SPARQLWrapper("http://data.bnf.fr/sparql")
 
 
-def query(entrydict, jointlevelkey, minus, number, uri=""):
-        
+def query(entrydict, jointlevelkey, minus, number, uri=""):      
     for i in range(len(pd.DataFrame(entrydict)[jointlevelkey].dropna())) :
         if pd.DataFrame(entrydict)[jointlevelkey].dropna()[i]['type'] == 'uri' : 
             sparql_query = """
@@ -44,8 +43,9 @@ def query(entrydict, jointlevelkey, minus, number, uri=""):
             entrydict[i]['children'] = []
         
 
-for uri in data['entite']:
+def uri2extract(uri):
     print(uri)
+    local_dict = {}
     sparql.setQuery("""
                     SELECT distinct *
                     WHERE
@@ -56,37 +56,62 @@ for uri in data['entite']:
     sparql.setReturnFormat(JSON)
     results = sparql.query().convert()
 
-    final_dict[uri] = results['results']['bindings']
+    local_dict[uri] = results['results']['bindings']
     list_df = []
     
     
-    query(final_dict[uri], 'level1', '1', '2', uri)
+    query(local_dict[uri], 'level1', '1', '2', uri)
     
-    for e in range(len(final_dict[uri])) :
-        if ("children" in final_dict[uri][e]
-            and len(final_dict[uri][e]['children']) > 0) :
-            query(final_dict[uri][e]['children'], 'level2', '2', '3', uri)
-            for f in range(len(final_dict[uri][e]['children'])):
-                if ("children" in final_dict[uri][e]['children'][f]
+    for e in range(len(local_dict[uri])) :
+        if ("children" in local_dict[uri][e]
+            and len(local_dict[uri][e]['children']) > 0) :
+            query(local_dict[uri][e]['children'], 'level2', '2', '3', uri)
+            for f in range(len(local_dict[uri][e]['children'])):
+                if ("children" in local_dict[uri][e]['children'][f]
                     and
-                    len(final_dict[uri][e]['children'][f]['children']) > 0) :
-                    query(final_dict[uri][e]['children'][f]['children'], 'level3', '3', '4', uri)
-                    for g in range(len(final_dict[uri][e]['children'][f]['children'])):
-                        if ("children" in final_dict[uri][e]['children'][f]['children'][g]
+                    len(local_dict[uri][e]['children'][f]['children']) > 0) :
+                    query(local_dict[uri][e]['children'][f]['children'], 'level3', '3', '4', uri)
+                    for g in range(len(local_dict[uri][e]['children'][f]['children'])):
+                        if ("children" in local_dict[uri][e]['children'][f]['children'][g]
                             and
-                            len(final_dict[uri][e]['children'][f]['children'][g]['children']) > 0) :
-                            query(final_dict[uri][e]['children'][f]['children'][g]['children'], 'level4', '4', '5', uri)
-                            for h in range(len(final_dict[uri][e]['children'][f]['children'][g]['children'])):
-                                if ("children" in final_dict[uri][e]['children'][f]['children'][g]['children'][h]
+                            len(local_dict[uri][e]['children'][f]['children'][g]['children']) > 0) :
+                            query(local_dict[uri][e]['children'][f]['children'][g]['children'], 'level4', '4', '5', uri)
+                            for h in range(len(local_dict[uri][e]['children'][f]['children'][g]['children'])):
+                                if ("children" in local_dict[uri][e]['children'][f]['children'][g]['children'][h]
                                     and
-                                    len(final_dict[uri][e]['children'][f]['children'][g]['children'][h]['children']) > 0) :
-                                    query(final_dict[uri][e]['children'][f]['children'][g]['children'][h]['children'], 'level5', '5', '6', uri)
+                                    len(local_dict[uri][e]['children'][f]['children'][g]['children'][h]['children']) > 0) :
+                                    query(local_dict[uri][e]['children'][f]['children'][g]['children'][h]['children'], 'level5', '5', '6', uri)
 
 
-dump = json.dumps(final_dict)
-o = open('sampleSPath.json', "w", encoding="utf-8")
-o.write(dump)
-o.close()
+
+def liste2split(data, i):
+    report_name = f'sampleSPath{str(i)}.json'
+    o = open(report_name, "w", encoding="utf-8")
+    dict_report = {}
+    max = split_param
+    if (len(data) < split_param):
+        max = len(data)
+    for uri in data['entite'][0:max]:
+        print(report_name, uri)
+        dict_report[uri] = uri2extract(uri)
+    dump = json.dumps(dict_report)
+    o.write(dump)
+    o.close()
+    with ZipFile(f'{report_name[:-5]+".zip"}', 'w') as myzip:
+        myzip.write(report_name)
+    remove(report_name)
+    if (len(data) > split_param):
+        liste2split(data[split_param:], i+1)
+    
+
+split_param = 2
+
+if __name__ == "__main__":
+    i = 1
+    liste2split(data, i)
+
+
+
 
 #for e in final_dict['http://data.bnf.fr/ark:/12148/cb119086682#about'] :
 #    query(final_dict['http://data.bnf.fr/ark:/12148/cb119086682#about'][e]['children'], 'level2', '2', '3')
